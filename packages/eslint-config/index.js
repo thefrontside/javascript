d459c7f05c634cd0ee5e9c6422dbd2a5daa4473e
Robbie Pitts <me@robbie-pitts.com>
2020-10-07 01:30:40 -0400

    Add new rules to eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -9,9 +9,18 @@ module.exports = {
   ],
   "rules": {
     "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/no-use-before-define": "off",
+    "arrow-spacing": "error",
+    "eol-last": "error",
+    "no-empty-function": "off",
+    "no-multi-spaces": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "one-var": ["error", { initialized: "never" }],
     "prefer-const": "off",
     "prefer-let/prefer-let": "error",
+    semi: ["error", "always", { omitLastInOneLineBlock: true }],
   }
 }
